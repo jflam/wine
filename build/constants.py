4a7273a0057e37a42ca728d5969d7108de95cc4f
John Lam <jlam@iunknown.com>
2021-11-10 03:58:21 +0000

    fix:updating index path to use persistent /data dir

--- build/constants.py
+++ build/constants.py
@@ -1,5 +1,5 @@
-NMS_INDEX1 = "./data/index.bin"
-NMS_INDEX2 = "./data/index.bin.dat"
+NMS_INDEX1 = "/data/index.bin"
+NMS_INDEX2 = "/data/index.bin.dat"
 SQLITE_DATASET = "./data/wine.db"
 CSV_DATASET = "./data/data.csv"
 SENTENCE_TRANSFORMER_MODEL_NAME = "msmarco-distilbert-base-v4"
