12c450a4627ed7afc68191c780f30dba54e420ec
John Lam <jlam@iunknown.com>
2021-11-18 05:16:24 +0000

    fix: updating location of wine.db to file share

--- build/constants.py
+++ build/constants.py
@@ -1,6 +1,6 @@
 NMS_INDEX1 = "/data/index.bin"
 NMS_INDEX2 = "/data/index.bin.dat"
-SQLITE_DATASET = "./data/wine.db"
+SQLITE_DATASET = "/data/wine.db"
 CSV_DATASET = "./data/data.csv"
 SENTENCE_TRANSFORMER_MODEL_NAME = "msmarco-distilbert-base-v4"
 LOCAL_MODEL_PATH = "~/.models/msmarco-distilbert-base-v4"
